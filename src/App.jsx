e8bd5ce7f1f6d479c3632278b37f8e0a164cb7b0
samrat-rock <samratpandey715@gmail.com>
2024-12-15 17:34:38 +0545

    myproflie

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+    <div className="text-3xl font-bold text-blue-500">
+      Hello, Tailwind CSS with Vite!
+    </div></>
+      
+    
+  )
+}
+
+export default App
